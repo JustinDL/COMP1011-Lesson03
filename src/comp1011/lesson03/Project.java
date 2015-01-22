634a3343207f26218493351e87dc32fd3a5f9862
JustinDL <justin_d_lucas@hotmail.com>
2015-01-22 16:25:08 -0500

    completed vehicle class

--- src/comp1011/lesson03/Project.java
+++ src/comp1011/lesson03/Project.java
@@ -14,8 +14,8 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+        Vehicle myVehicle = new Vehicle();
+        myVehicle.
         
         
     }
--- src/comp1011/lesson03/Vehicle.java
+++ src/comp1011/lesson03/Vehicle.java
@@ -10,33 +10,67 @@ package comp1011.lesson03;
  * 
  */
 public class Vehicle {
-    int numWheels = 4;
-    int numDoors = 2;
-    String colour = "";
-    String make ="";
-    String model ="";
-    String year ="";
-    float speed = 0;
+    private int numWheels = 4;
+    private int numDoors = 2;
+    private String colour = "";
+    private String make ="";
+    private String model ="";
+    private String year ="";
+    private float speed = 0;
+    private final int MAX_SPEED = 40;
     
+    // constructor
     
     public Vehicle(){
-        
-        
-    }
-    
-    public void accelerate(){
-        
-    }
-    public void decelerate(){
           
     }
+    
+    
+        // getters and setters
+    public int getNumwheels(){
+        return this.numWheels;
+    }
+    public int getNumDoors(){
+        return this.numDoors;
+    }
+    public String getColour(){
+        return this.colour;
+    }
+    public String getMake(){
+        return this.make;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public String getYear(){
+        return this.year;
+    }
+    public float getSpeed(){
+        return this.speed;
+    }
+    public int getMAXSPEED(){
+        return this.MAX_SPEED;
+    }
+    
+    // public methods
+    public void accelerate(){
+        this.speed += 4;
+        if(this.speed >= this.MAX_SPEED){
+            this.speed = this.MAX_SPEED;
+        }
+    }
+    public void decelerate(){
+        this.speed -= 6;
+        if (this.speed < 0){
+            this.speed = 0;
+        }
+    }
     public void turnLeft(){
-        
+        System.out.println("you are turning left");
     }
     public void turnRight(){
-        
+        System.out.println("you are tuning right");
     }
     
-    
-    
+
 }
