f15bbc8e222b18ff53374c122c32b5d5fd3e7c29
JustinDL <justin_d_lucas@hotmail.com>
2015-01-22 16:00:53 -0500

    Empty vehicle class

---
+++ src/comp1011/lesson03/Vehicle.java
@@ -0,0 +1,42 @@
+package comp1011.lesson03;
+
+
+
+/**
+ *
+ * @author Justin
+ * Class description: creating a vehicle class to use as a
+ * superclass for all oher vehicle subclasses
+ * 
+ */
+public class Vehicle {
+    int numWheels = 4;
+    int numDoors = 2;
+    String colour = "";
+    String make ="";
+    String model ="";
+    String year ="";
+    float speed = 0;
+    
+    
+    public Vehicle(){
+        
+        
+    }
+    
+    public void accelerate(){
+        
+    }
+    public void decelerate(){
+          
+    }
+    public void turnLeft(){
+        
+    }
+    public void turnRight(){
+        
+    }
+    
+    
+    
+}
